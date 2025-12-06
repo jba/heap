be95f4756a4392ffd1ae6d46066956f045a2ee8d
Jonathan Amsterdam <jba@google.com>
2025-12-06 12:04:58 -0500

    benchmark Heap vs HeapFunc (Claude)

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,7 @@
 package heap
 
 import (
+	"cmp"
 	"math/rand"
 	"testing"
 )
@@ -28,6 +29,37 @@ func BenchmarkInsert(b *testing.B) {
 			h := New[int]()
 			for _, n := range nums {
 				h.InsertItem(n)
+			}
+			for h.Len() > 0 {
+				h.ExtractMin()
+			}
+		}
+	})
+}
+
+func BenchmarkHeapVsHeapFunc(b *testing.B) {
+	nums := make([]int, 1000)
+	for i := range nums {
+		nums[i] = rand.Int()
+	}
+
+	b.Run("Heap", func(b *testing.B) {
+		for b.Loop() {
+			h := New[int]()
+			for _, n := range nums {
+				h.Insert(n)
+			}
+			for h.Len() > 0 {
+				h.ExtractMin()
+			}
+		}
+	})
+
+	b.Run("HeapFunc", func(b *testing.B) {
+		for b.Loop() {
+			h := NewFunc(cmp.Compare[int])
+			for _, n := range nums {
+				h.Insert(n)
 			}
 			for h.Len() > 0 {
 				h.ExtractMin()
